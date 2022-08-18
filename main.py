3d7cc47f43d64180f2272744e144df4aa734adf2
Anthony-S58 <tonysim01@hotmail.com>
2022-08-18 23:28:57 +0200

    Text to Ascii

--- main.py
+++ main.py
@@ -4,6 +4,16 @@
 #import de la fonction random
 import random
 
+#text to ascii for visual in shell
+print("""  _____  _             _____                      
+ |  __ \(_)           / ____|                     
+ | |  | |_  ___ ___  | |  __  __ _ _ __ ___   ___ 
+ | |  | | |/ __/ _ \ | | |_ |/ _` | '_ ` _ \ / _ \`
+ | |__| | | (_|  __/ | |__| | (_| | | | | | |  __/
+ |_____/|_|\___\___|  \_____|\__,_|_| |_| |_|\___|
+                                                  
+                                                  """)
+
 #variables et utilisation de l'aléatoire avec random
 player = random.randint(1,20)
 print("Vous avez obtenu : " + str(player))
