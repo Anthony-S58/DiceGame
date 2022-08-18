5b7a19b160e88bb05ee57bb666f424f2343136b6
Anthony-S58 <tonysim01@hotmail.com>
2022-08-18 22:58:05 +0200

    Add Victory condition

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ import random
 #variables et utilisation de l'aléatoire avec random
 player = random.randint(1,20)
 ai = random.randint(1,20)
+
+#condition de victoire
+if player > ai :
+    print("Vous avez gagné !")
+else :
+    print("Vous avez perdu !")
